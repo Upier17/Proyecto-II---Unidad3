d57f0bdade906782bc79bd7e93c223cb55f6fdc8
Upier <114083071+Upier17@users.noreply.github.com>
2024-03-14 04:08:33 -0600

    Add files via upload

---
+++ Unidad 3/Codigo de proyecto/script.js
@@ -0,0 +1,11 @@
+function createAccount() {
+    alert("Cuenta creada existosamente.");
+}
+
+function login() {
+    alert("Login successful.");
+}
+
+function recoverPassword() {
+    alert("Password recovery email sent.");
+}
